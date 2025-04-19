73a3bdf41e14d78331c4a8aaa2e44cd894fe0529
nithinmanayilghub <nithinmamanayil@gmail.com>
2025-04-19 20:57:29 +0530

    Add Solutions to Day 29 Problems Number of Islands And Burst Balloons

---
+++ Day 29.py
@@ -0,0 +1,63 @@
+# 200. Number of Islands
+# https://leetcode.com/problems/number-of-islands/description/?envType=problem-list-v2&envId=array
+# Approach - Breadth First Search
+import collections
+from typing import List
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid:
+            return 0
+        rows, cols = len(grid), len(grid[0])
+        visit = set()
+        islands = 0
+
+        def bfs(r, c):
+            q = collections.deque()
+            visit.add((r, c))
+            q.append((r, c))
+            while q:
+                row, col = q.popleft()
+                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+                for dr, dc in directions:
+                    r, c = row + dr, col + dc
+                    if (
+                        r in range(rows)
+                        and c in range(cols)
+                        and grid[r][c] == "1"
+                        and (r, c) not in visit
+                    ):
+                        q.append((r, c))
+                        visit.add((r, c))
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r, c) not in visit:
+                    bfs(r, c)
+                    islands += 1
+        return islands
+
+
+####################################################################################################
+## 312. Burst Balloons
+## https://leetcode.com/problems/burst-balloons/description/?envType=problem-list-v2&envId=array
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
+        nums = [1] + nums + [1]
+        dp = {}
+
+        def dfs(l, r):
+            if l > r:
+                return 0
+            if (l, r) in dp:
+                return dp[(l, r)]
+
+            dp[(l, r)] = 0
+            for i in range(l, r + 1):
+                coins = nums[l - 1] * nums[i] * nums[r + 1]
+                coins += dfs(l, i - 1) + dfs(i + 1, r)
+                dp[(l, r)] = max(dp[(l, r)], coins)
+            return dp[(l, r)]
+
+        return dfs(1, len(nums) - 2)
